b37c098fe649377f8ebb9570d49994450c8e7785
rajul007 <archiegarg100@gmail.com>
2023-08-26 14:41:28 +0530

    Add TextForm Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
 
 function App() {
   return (
     <>
-     <Navbar title="TextUtils"/> 
+      <Navbar title="TextUtils" />
+      <TextForm heading="Enter the text to analyze below" />
     </>
   );
 }
---
+++ src/components/TextForm.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react'
+import PropTypes from "prop-types"
+
+export default function TextForm(props) {
+    const [text, setText] = useState('');
+    const wpm = 0.004;
+
+    const handleOnChange = (event) => {
+        setText(event.target.value);
+    }
+
+    const handleUpperCase = () => {
+        setText(text.toUpperCase());
+    }
+
+    const handleLowerCase = () => {
+        setText(text.toLowerCase());
+    }
+
+    const handleExtraSpaces = () => {
+        setText(text.split(/[ ]+/).join(" "));
+    }
+
+    const handleCopy = () => {
+        navigator.clipboard.writeText(text);
+    }
+
+    const handleClear = () => {
+        setText("");
+    }
+
+    const wordCount = (text) => text === "" ? 0 : text.split(" ").length;
+
+    return (
+        <div className='container my-3'>
+            <div className="container">
+                <h2 className='my-3'>{props.heading}</h2>
+                <div className="mb-3">
+                    <textarea className="form-control" placeholder='Enter text here' value={text} onChange={handleOnChange} id="myBox" rows="5"></textarea>
+                </div>
+                <button className="btn btn-primary mx-1" onClick={handleUpperCase}>Uppercase</button>
+                <button className="btn btn-primary mx-1" onClick={handleLowerCase}>Lowercase</button>
+                <button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>Remove Extra Space</button>
+                <button className="btn btn-primary mx-1" onClick={handleCopy}>Copy</button>
+                <button className="btn btn-primary mx-1" onClick={handleClear}>Clear</button>
+            </div>
+            <div className="container my-3">
+                <h3>Text Summary</h3>
+                <p>{wordCount(text)} words and {text.length} characters</p>
+                <p>{Math.floor(wpm * wordCount(text))} minutes and {((wpm * wordCount(text) - Math.floor(wpm * wordCount(text))) * 60).toFixed(2)} seconds to read</p>
+                <h3>Preview</h3>
+                <pre>{text}</pre>
+            </div>
+        </div>
+    )
+}
+
+TextForm.propTypes = {
+    heading: PropTypes.string.isRequired,
+};
+
+TextForm.defaultProps = {
+    heading: "Enter the text below",
+};
