47d3787ddb72f27ede2051e9789996df7f16892a
rajul007 <archiegarg100@gmail.com>
2023-08-28 11:01:07 +0530

    Minor Modifications

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
   }
   return (
     <>
-      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
+      <Navbar title="TextAlchemy" mode={mode} toggleMode={toggleMode}/>
       <TextForm heading="Enter the text to analyze below" mode={mode}/>
     </>
   );
